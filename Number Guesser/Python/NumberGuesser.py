e1acaee6fb27e03681760b395440f8e9e587b30c
AyliasTheCoder <AyliasTheCoder@protonmail.com>
2022-08-24 12:37:12 -0600

    Fixed grammar/spelling in NumberGuesser.py

--- Number Guesser/Python/NumberGuesser.py
+++ Number Guesser/Python/NumberGuesser.py
@@ -1,4 +1,4 @@
-print("think of a number between 1 and 1000")
+print("Think of a number between 1 and 1000")
 
 top: int = 1000
 bottom: int = 1
@@ -9,7 +9,7 @@ while True:
     guess: int = (top + bottom) // 2
 
     print("My guess is " + str(guess))
-    correct: str = input("was i correct? (h if your number is higher, l if its lower, and c if it is correct)\n")
+    correct: str = input("Was I correct? (h if your number is higher, l if its lower, and c if it is correct)\n")
 
     count += 1
 
@@ -21,4 +21,5 @@ while True:
         print("I guessed your number in " + str(count) + " guesses!")
         break
     else:
-        print("I didn't understand")
+        print("I didn't understand, please try again!")
+        count -= 1
