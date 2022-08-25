155266b752079503bec8e9bc32df39a11b29dfa5
AyliasTheCoder <AyliasTheCoder@protonmail.com>
2022-08-25 06:32:59 -0600

    Fixed print statements in NumberGuesser.py

--- Number Guesser/Python/NumberGuesser.py
+++ Number Guesser/Python/NumberGuesser.py
@@ -8,7 +8,7 @@ count: int = 0
 while True:
     guess: int = (top + bottom) // 2
 
-    print("My guess is " + str(guess))
+    print("My guess is ", str(guess))
     correct: str = input("Was I correct? (h if your number is higher, l if its lower, and c if it is correct)\n")
 
     count += 1
@@ -18,7 +18,7 @@ while True:
     elif correct == 'l':
         top = guess
     elif correct == 'c':
-        print("I guessed your number in " + str(count) + " guesses!")
+        print("I guessed your number in", count, "guesses!")
         break
     else:
         print("I didn't understand, please try again!")
