6ace1fd102b2581b522e44eb821dbdecc5f06100
AyliasTheCoder <AyliasTheCoder@protonmail.com>
2022-08-26 09:03:48 -0600

    Added proper main function to number_guesser.py

--- Number Guesser/Python/number_guesser.py
+++ Number Guesser/Python/number_guesser.py
@@ -1,25 +1,29 @@
-print("Think of a number between 1 and 1000")
+def main():
+    print("Think of a number between 1 and 1000")
 
-top: int = 1000
-bottom: int = 1
+    top: int = 1000
+    bottom: int = 1
 
 
-count: int = 0
-while True:
-    guess: int = (top + bottom) // 2
+    count: int = 0
+    while True:
+        guess: int = (top + bottom) // 2
 
-    print("My guess is ", str(guess))
-    correct: str = input("Was I correct? (h if your number is higher, l if its lower, and c if it is correct)\n")
+        print("My guess is ", str(guess))
+        correct: str = input("Was I correct? (h if your number is higher, l if its lower, and c if it is correct)\n")
 
-    count += 1
+        count += 1
 
-    if correct == 'h':
-        bottom = guess
-    elif correct == 'l':
-        top = guess
-    elif correct == 'c':
-        print("I guessed your number in", count, "guesses!")
-        break
-    else:
-        print("I didn't understand, please try again!")
-        count -= 1
+        if correct == 'h':
+            bottom = guess
+        elif correct == 'l':
+            top = guess
+        elif correct == 'c':
+            print("I guessed your number in", count, "guesses!")
+            break
+        else:
+            print("I didn't understand, please try again!")
+            count -= 1
+
+if __name__ == '__main__':
+    main()
