b2bfb758ee13309d537c88c048a0e662c34be774
AyliasTheCoder <AyliasTheCoder@protonmail.com>
2022-08-26 09:02:49 -0600

    Added proper main function to thousand_primes.py

--- Calculate First 1000 Primes/Python/thousand_primes.py
+++ Calculate First 1000 Primes/Python/thousand_primes.py
@@ -8,12 +8,16 @@ def is_prime(number):
     return True
 
 
-primes: list = [2, 3, 5, 7, 11]
-i: int = 12
-while len(primes) < 1000:
-    if is_prime(i):
-        primes.append(i)
-    i += 1
+def main():
+    primes: list = [2, 3, 5, 7, 11]
+    i: int = 12
+    while len(primes) < 1000:
+        if is_prime(i):
+            primes.append(i)
+        i += 1
 
-for prime in primes:
-    print(prime)
+    for prime in primes:
+        print(prime)
+
+if __name__ == '__main__':
+    main()
